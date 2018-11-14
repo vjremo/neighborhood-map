4cfdd9c7c2af0ac1fb842ad7128315ead762096d
Vijay G <vjremo@gmail.com>
2018-11-14 14:08:16 -0500

    feat : add list bar of restaurants

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import locations from './data/locations.json'
 import DisplayMap from './components/DisplayMap'
+import DisplayListDrawer from './components/DisplayListDrawer'
+
 class App extends Component {
 
   state = {
@@ -9,14 +11,42 @@ class App extends Component {
       lat: 40.031162, 
       lon: -82.912036,
       zoom: 17,
-      all : locations
+      all : locations,
+      open: false
 
+  }
+  styles = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: "absolute",
+      left: 10,
+      top: 20,
+      background: "black",
+      padding: 10
+    },
+    hide: {
+      display: 'none'
+    },
+    header: {
+      marginTop: "0px"
+    }
+  };
+
+  toggleDrawer = () => {
+    // Toggle the value controlling whether the drawer is displayed
+    this.setState({
+      open: !this.state.open
+    });
   }
 
   render() {
     return (
       <div className="App">
         <div>
+        <button onClick={this.toggleDrawer} style={this.styles.menuButton}>
+            <i className="fas fa-bars"></i>
+          </button>
           <h1>Columbus, OH Restaurants</h1>
         </div>
         <DisplayMap
@@ -24,6 +54,10 @@ class App extends Component {
           lon={this.state.lon}
           zoom={this.state.zoom}
           locations={this.state.all}/>
+          <DisplayListDrawer
+            locations={this.state.all}
+            open={this.state.open}
+            toggleDrawer={this.toggleDrawer}/>
       </div>
     );
   }
