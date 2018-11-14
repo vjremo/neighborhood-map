52c58aaaca82413a6e775868e5223bbd118e2da3
Vijay G <vjremo@gmail.com>
2018-11-13 23:36:06 -0500

    feat : add marker and infowindow components

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 
 const MAP_KEY = "AIzaSyAZUdiGhHDhXMKF5ZfKuF5vbFQI_sKpWVo"
 
 class DisplayMap extends Component{
     state ={
-        map : null
+        map : null,
+        markers : [],
+        mapMarkers : [],
+        activeMarker : null,
+        activeMarkerProps : null,
+        showingInfoWindow : false
     }
 
     componentDidMount = () =>{
@@ -15,6 +20,63 @@ class DisplayMap extends Component{
     mapReady = (props, map) => {
         //Save the map reference in state and prepare the location markers
         this.setState(map);
+        this.updateMarkers(this.props.locations)
+    }
+
+    closeInfoWindow = () => {
+        //Display any active marker animation
+        this.state.activeMarker && this
+        .state
+        .activeMarker
+        .setAnimation(null);
+        this.setState({showingInfoWindow: false,
+        activeMarker: null,
+        activeMarkerProps: null })
+    }
+
+    onMarkerClick = () => {
+        //Close any info window already open
+        this.closeInfoWindow();
+    }
+
+    updateMarkers = (locations) =>{
+        //If all locations are filtered, then we're done
+        if(!locations){
+            return;
+        }
+
+        //Remove any existing markers from map
+        this
+            .state
+            .markers
+            .forEach(marker=>marker.setMap(null));
+        
+        /*Iterate over locations to create parallel references to marker properties
+        and mark themselves that can be used for reference in interactions.
+        And add the markers to map along the way*/
+        let markerProps= []
+        let markers = locations.map((location,index)=>{
+            let mProps = {
+                key: index,
+                index,
+                name : location.name,
+                position : locations.pos,
+                url : location.url
+            }
+            markerProps.push(mProps)
+            
+            let animation = this.props.google.maps.Animation.DROP;
+            let marker = new this.props.google.maps.Marker({
+                position: location.pos,
+                map : this.state.map,
+                animation
+            })
+            marker.addListener('click', ()=>{
+                this.onMarkerClick(mProps, marker, null)
+            })
+            return marker
+        })
+        this.setState({markers, markerProps})
     }
 
     render = () => {
@@ -27,6 +89,9 @@ class DisplayMap extends Component{
             lng : this.props.lon
         }
 
+        let amProps = this.state.activeMarkerProps
+
+        
         return (
             <Map 
                 role="application"
@@ -36,8 +101,21 @@ class DisplayMap extends Component{
                 zoom={this.props.zoom}
                 style={style}
                 initialCenter={center}
-                onClick={this.closeInfoWindow}
-                ></Map>
+                onClick={this.closeInfoWindow}>
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onClose={this.state.closeInfoWindow}>
+                    <div>
+                        <h3>{amProps && amProps.name}</h3>
+                        {amProps && amProps.url 
+                        ? (
+                            <a href={amProps.url}>See website</a>
+                        ) : ""}
+                    </div>
+                    </InfoWindow>
+                </Map>
+                
 
         )
     }
